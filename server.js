356834434b81b7d1ffe9a8b097a0b446355ec90d
Tristan Bond <tristanebond@gmail.com>
2021-02-11 19:44:22 -0800

    inital html routes and api routes skeleton

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.use(require("./routes/html-routes"));
+app.use(require("./routes/api-routes"))
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populate", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 
 app.listen(PORT, () => {
