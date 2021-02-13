718f383e59eeaa23c7cb0a2d57b3f8efc6f5097a
Tristan Bond <tristanebond@gmail.com>
2021-02-12 19:42:09 -0800

    connecting to heroku

--- server.js
+++ server.js
@@ -14,7 +14,10 @@ app.use(express.static("public"));
 app.use(require("./routes/html-routes"));
 app.use(require("./routes/api-routes"))
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false
+});
 
 
 app.listen(PORT, () => {
